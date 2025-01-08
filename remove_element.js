42fea7c056903c9768599594e0546a8d00bef685
Ashik275 <karzon275@gmail.com>
2025-01-08 20:18:20 +0600

    remove element

---
+++ remove_element.js
@@ -0,0 +1,20 @@
+function removeElement(nums, val) {
+    let count = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[count] = nums[i];  // Move non-val elements to the front
+            count++;
+        }
+    }
+
+    return count;
+}
+
+// Example usage:
+let nums = [0, 1, 2, 2, 3, 0, 4, 2];
+let val = 2;
+
+let k = removeElement(nums, val);
+console.log(k);  // Output: 5
+console.log(nums.slice(0, k));
